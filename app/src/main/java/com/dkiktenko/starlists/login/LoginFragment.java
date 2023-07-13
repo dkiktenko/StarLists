af3ec3300a48add0fb6c7e041c95ba4becb9292e
Darya Kiktenko <dkiktenko@hotmail.com>
2023-07-12 20:59:49 -0500

    Added login authentication

--- app/src/main/java/com/dkiktenko/starlists/login/LoginFragment.java
+++ app/src/main/java/com/dkiktenko/starlists/login/LoginFragment.java
@@ -2,9 +2,13 @@ package com.dkiktenko.starlists.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,12 +19,18 @@ import androidx.fragment.app.FragmentTransaction;
 import com.dkiktenko.starlists.BugReport.ReportABugActivity;
 import com.dkiktenko.starlists.MainActivity;
 import com.dkiktenko.starlists.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginFragment extends Fragment {
 
+    static final String TAG = "LoginFragment";
+
     private final View.OnClickListener loginButtonClickHandler = v -> {
-        // TODO: Implement authentication
-        openAppEntryPoint();
+        validateCredentials();
     };
     private final View.OnClickListener forgotPasswordClickHandler = view -> {
         // TODO: Implement showing of the "Forgot Password" fragment
@@ -33,6 +43,10 @@ public class LoginFragment extends Fragment {
                 .commit();
     };
 
+    private @NonNull TextInputEditText emailField;
+    private @NonNull TextInputEditText passwordField;
+    private @NonNull FirebaseAuth mAuth;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -43,22 +57,40 @@ public class LoginFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
-        if (validateCredentials()) {
-            openAppEntryPoint();
-            return;
-        }
+        mAuth = FirebaseAuth.getInstance();
+
+        emailField = ViewCompat.requireViewById(view, R.id.email);
+        passwordField = ViewCompat.requireViewById(view, R.id.password);
 
         setupReportABug(view);
-
         setupLoginButton(view);
         setupForgotPasswordButton(view);
         setupSignUpButton(view);
     }
 
 
-    private boolean validateCredentials() {
-        // TODO: Implement validation of credentials if user is already signed in
-        return false;
+    private void validateCredentials() {
+        Editable email = emailField.getText();
+        Editable password = passwordField.getText();
+
+        if (TextUtils.isEmpty(email) ||
+                TextUtils.isEmpty(password)) {
+            Log.w(TAG, "Email and/or password left empty");
+            Toast.makeText(requireContext(), R.string.email_password_cannot_be_empty_warning, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email.toString(), password.toString()).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d(TAG, "signInWithEmail:success");
+                openAppEntryPoint();
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                Toast.makeText(requireContext(), R.string.email_password_incorrect, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void setupReportABug(@NonNull View view) {
